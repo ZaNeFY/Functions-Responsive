312aa7b445f1d44b41ca0113fdf165a4e4213bd1
david bzhinava <david.bzhinava1@gmail.com>
2024-01-23 18:46:21 -0800

    six commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// function misalmeba(saxeli, asaki) {
+//     alert(`გამარჯობა ${saxeli}, შენ ხარ ${asaki} `);
+// }
+
+// let saxeli = prompt("შეიყვანეთ სახელი");
+// let asaki = prompt("შეიყვანეთ ასაკი");
+
+// misalmeba(saxeli, asaki);
+
+// function mimateba(a, b) {
+//     let plus = a + b;
+//     return plus
+// }
+// console.log(mimateba(2,5))
+
+// let globaluri = "me var globaluri cvladi"
+
+// function testFunction() {
+//     console.log(globaluri);
+// }
+
+// testFunction()
+// console.log(globaluri)
+
+// function testLocal() {
+//     let lokaluri = "me var lokaluri cvladi"
+//     console.log(lokaluri)
+// }
+
+// testLocal()
+
+// function gamravleba(a, b ,c) {
+//     return a * b * c
+// }
+
+// let pasuxi = gamravleba(4,1,2)
+
+// console.log(pasuxi)
+
+// ARROW FUNCTIONS
+let gamravleba = (x, y) => x * y;
+
+let pasuxi = gamravleba(3, 4);
+
+console.log(pasuxi);
